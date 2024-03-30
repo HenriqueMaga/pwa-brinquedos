99e81e883a224dcc340331fd5f5e0157dd084ff0
Henrique Magalhães <henrique.araujo@saint-gobain.com>
2024-03-30 09:35:59 -0300

    Service Worker gerados e salvos no cache da aplicação

---
+++ serviceWorker.js
@@ -0,0 +1,46 @@
+`use strict`;
+
+const CACHE_NAME = `briquedo-app-estatico`;
+
+const FILES_TO_CACHE = [
+    `css/bootstrap.min.css`,
+    `css/style.css`,
+    `icons/favicon.ico`,
+    `icons/152.png`,
+    `imgs/logo.png`,
+    `imgs/offline.png`,
+    `imgs/bg001.jpg`,
+    `imgs/bg002.jpg`,
+    `imgs/cat_icon.jpg`,
+    `js/app.js`,
+    `js/bootstrap.bundle.min.js`,
+    `offline.html`
+];
+
+self.addEventListener(`install`, (event) => {
+    console.log(`Service Worker em instalação`);
+
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log(`adicionando arquivos no cache estatico`);
+            console.log(FILES_TO_CACHE);
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener(`activate`, (event) =>{
+    console.log(`Service Worker em ativação`);
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) =>{
+                if(key !== CACHE_NAME){
+                    return caches.delete(key);
+                }
+            }))
+        })
+    );
+
+    self.clients.claim();
+});
